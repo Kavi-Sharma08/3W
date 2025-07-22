4701008f4033f646b0356aa66566cd7e1765a148
Kavi <thekavisharma26@gmail.com>
2025-07-22 16:26:13 +0530

    added backend url in websocket origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chipper-starlight-5e6465.netlify.app",
     methods: ["GET", "POST"],
   },
 });
