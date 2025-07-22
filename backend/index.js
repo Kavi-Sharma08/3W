1d85824c142c1e68a0799ddd6a3b62bf01bd8a20
Kavi <thekavisharma26@gmail.com>
2025-07-22 16:45:44 +0530

    bug in socket

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ dotenv.config();
 const app = express();
 app.use(express.json()); 
 app.use(cors({
-    origin : "https://chipper-starlight-5e6465.netlify.app"
+    origin : "https://chipper-starlight-5e6465.netlify.app",
+    credentials : true
 }))
 
 app.use("/" , authLogin);
